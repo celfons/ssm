6d665743b128d5ee1310419452e564fc6ccb8174
Marcel Fonseca <marcelfons@hotmail.com>
2024-08-11 23:40:39 -0300

    Atualizar o main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "foo" {
-  name  = "/myapp/FOO"
+  name  = "${var.parameter_store_name}/FOO"
   type  = bool
   value = true
 }
 
 resource "aws_ssm_parameter" "bar" {
-  name  = "/myapp/BAR"
+  name  = "${var.parameter_store_name}/BAR"
   type  = bool
   value = false
 }

8b13236f3939f850963fd2b700817356dca309f9
marcel <marcelfons@gmail.com>
2024-08-12 08:51:54 -0300

    alterado main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "foo" {
   name  = "${var.parameter_store_name}/FOO"
-  type  = bool
-  value = true
+  type  = "String"
+  value = "true"
 }
 
 resource "aws_ssm_parameter" "bar" {
   name  = "${var.parameter_store_name}/BAR"
-  type  = bool
-  value = false
+  type  = "String"
+  value = "false"
 }
